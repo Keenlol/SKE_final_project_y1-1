492f1ba22e0e3ba474315e09b1578769e6eeb31a
Chetphisuth Tongpa <chetphisuth.t@ku.th>
2024-12-14 23:22:18 +0700

    make button handle clicks

--- button.py
+++ button.py
@@ -8,7 +8,7 @@ class Button(Text):
         self.idle_color = idle_color
         self.extra_size = 0
 
-    def __get_cursor_pos(self, canvas):
+    def __get_cursor_pos(self):
         screen = turtle.Screen()
         cursor_x = screen.getcanvas().winfo_pointerx() - screen.getcanvas().winfo_rootx()
         cursor_y = screen.getcanvas().winfo_pointery() - screen.getcanvas().winfo_rooty()
@@ -18,18 +18,21 @@ class Button(Text):
         
         return cursor_x, cursor_y
 
-    def draw_animation(self):
-        canvas = turtle.getcanvas()
-        cursor_x, cursor_y = self.__get_cursor_pos(canvas)
-
+    def is_hovered(self, x, y):
         bottom_right = [self.char_list[0].grid_points[8][0] + self.extra_size,
                        self.char_list[0].grid_points[8][1] - self.extra_size]
         top_left = [self.char_list[-1].grid_points[0][0] - self.extra_size,
                     self.char_list[-1].grid_points[0][1] + self.extra_size]
+        
+        return bottom_right[0] <= x <= top_left[0] and bottom_right[1] <= y <= top_left[1]
 
-        if bottom_right[0] <= cursor_x <= top_left[0] and bottom_right[1] <= cursor_y <= top_left[1]:
+    def active(self):
+        cursor_x, cursor_y = self.__get_cursor_pos()
+        
+        if self.is_hovered(cursor_x, cursor_y):
             self.color = self.hover_color
         else:
             self.color = self.idle_color
         
         self.draw()
+

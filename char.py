4d6711a7f0a6eef201022d569c2957c3a2e207f6
Chetphisuth Tongpa <chetphisuth.t@ku.th>
2024-12-14 11:51:53 +0700

    handle space " " character

--- char.py
+++ char.py
@@ -42,7 +42,9 @@ class Char:
                           "W": [0,6,4,8,2],
                           "X": [0,8,4,2,6],
                           "Y": [0,4,2,4,7],
-                          "Z": [0,2,6,8]}
+                          "Z": [0,2,6,8],
+                          " ": [],
+                          "_": [6,8]}
         # 0  1  2
         # 3  4  5
         # 6  7  8
@@ -69,16 +71,14 @@ class Char:
         turtle.pensize(self.thickness)
         turtle.goto(self.x, self.y)
         turtle.setheading(0)
-        turtle.pendown()
 
         sequence = self.__draw_seq[char]
-        turtle.penup()
-        turtle.goto(self.grid_points[sequence[0]][0], self.grid_points[sequence[0]][1])
-        turtle.pendown()
-        for i in range(1, len(sequence)):
-            turtle.goto(self.grid_points[sequence[i]][0], self.grid_points[sequence[i]][1])
-            print("goto:", self.grid_points[sequence[i]][0], self.grid_points[sequence[i]][1])
-        turtle.penup()
+        if not sequence == []:
+            turtle.goto(self.grid_points[sequence[0]][0], self.grid_points[sequence[0]][1])
+            turtle.pendown()
+            for i in range(1, len(sequence)):
+                turtle.goto(self.grid_points[sequence[i]][0], self.grid_points[sequence[i]][1])
+            turtle.penup()
     
     def __str__(self) -> str:
         return f"digit pos=({self.x:.2f}, {self.y:.2f})"
